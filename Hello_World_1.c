32f34ee7ba9af61c8362628194f03b9334a55fb1
Kemal <131789022+kemalbankoglu@users.noreply.github.com>
2023-09-19 00:46:07 +0300

    Update Hello_World_1.c

--- Hello_World_1.c
+++ Hello_World_1.c
@@ -10,8 +10,7 @@ int main()
     scanf("%[^\n]%*c", &s);
     
     printf("Hello, World!\n");
-    printf("%s",s);
-  	
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    printf("%s",s);   
+	
     return 0;
 }
